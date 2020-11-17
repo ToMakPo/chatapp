f693f98f4f00323b1cd0a0178db183a1b491af50
Makai Post <postmakai@gmail.com>
2020-11-17 05:08:22 -0800

    first commit

---
+++ link.js
@@ -0,0 +1,65 @@
+var socket
+
+var senderNameInput = $('#sender-name-input')
+var messageInput = $('#message-input')
+var log = $('#log')
+
+var senderName
+
+function init() {
+
+    $('#connect-button').on('click', event => {
+        let key = $('#connect-to').val()
+        if (key) connectTo(key)
+    })
+
+    $('#send-button').on('click', event => {
+        let msg = messageInput.val()
+        if (msg) sendMsg(msg)
+        messageInput.val('')
+    })
+
+    senderName = localStorage.getItem('sender-name') || ''
+    senderNameInput.val(senderName)
+        .on('blur', event => {
+            senderName = senderNameInput.val()
+            localStorage.setItem('sender-name', senderName)
+        })
+
+    setup()
+}
+
+function connectTo(key) {
+    console.log('connecting to: ', key);
+}
+
+function sendMsg(msg) {
+    console.log('sending msg: ', msg);
+
+    var data = {
+        senderName: senderName,
+        msg: msg,
+        timestamp: moment()
+    }
+    socket.emit('send-msg', data)
+
+    logMsg(data)
+}
+
+function setup() {
+    let path = 'http://localhost:3000'
+    socket = io.connect(path)
+    socket.on('send-msg', data => {
+        logMsg(data)
+    })
+}
+
+function logMsg(data) {
+    let cells = [
+        $('<td>').text(moment(data.timestamp).format('MM-DD-YYYY HH:mm:ss')),
+        $('<td>').text(data.senderName),
+        $('<td>').text(data.msg)
+    ]
+    let row = $('<tr>').append(cells)
+    log.append(row)
+}
